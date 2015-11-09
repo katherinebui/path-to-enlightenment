ed4aeb1385faec8ca8705469ae5e527524133ccc
Kooshaba <kooshaba@gmail.com>
2015-11-08 20:25:50 -0800

    triangle is working!

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,20 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  
+	sides = [a, b, c].sort
+	max = sides.pop
+	sum = sides.inject(&:+) # & makes what follows expected to be a Proc. symbols (like :+) have an inherent to_proc method
+	# :+ == Proc.new { |x, y| x + y }
+
+	raise TriangleError unless sum > max
+
+  if a == b && b == c # & does not short circuit ie: it executes both conditionals
+  	:equilateral # && both has to be true for it to return true, if one false, will return false
+	elsif a == b || b == c || a == c # || short circuits, cuts off if the first conditional is false
+		:isosceles
+	else
+		:scalene
+	end
 end
 
 # Error class used in part 2.  No need to change this code.
